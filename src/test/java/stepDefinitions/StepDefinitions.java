3e780468b2fc59602a16fee8fcb84061b023423e
Saidahmad <rahmonov1001@gmail.com>
2020-03-12 00:16:22 -0700

    This is second commit after some changes from American guy

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -88,6 +88,7 @@ import java.util.List;
         System.out.println("First line from Indian guy");
         System.out.println("Second line from Indian guy");
         System.out.println("Third line from Indian guy");
+        System.out.println("First line from American guy");
 
     }
 
